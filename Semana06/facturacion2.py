0e81eec09af6044a09cbab2ee5babb3560f3b4ce
Miguel Angel Prado Zuta <mikk_pr@hotmail.com>
2022-10-26 09:41:02 -0500

    Semana 06 - Ejemplo: Camelcase

--- Semana06/facturacion2.py
+++ Semana06/facturacion2.py
@@ -11,6 +11,6 @@ import financieros
 
 total = 1000.49
 
-print(f"Subtotal: {financieros.obtenerSubtotalconTotal(total): .2f}")
+print(f"Subtotal: {financieros.obtenerSubtotalconTotal(total):.2f}")
 print(f"IGV:{financieros.obtenerIGVconTotal(total):.2f}")
 print(f"Total:{total}")
